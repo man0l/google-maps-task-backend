25d6b21dba73e56ec86e831d8038e38282b39124
Manol Trendafilov <manol.trendafilov@gmail.com>
2020-03-03 19:48:23 +0200

    install monolog logger

--- src/Controller/MapsController.php
+++ src/Controller/MapsController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
+use App\Services\Maps\OpenStreetMaps;
+use App\Services\Maps\GoogleMaps;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MapsController extends AbstractController
 {
     /**
-     * @Route("/maps", name="maps")
+     * @Route("/maps/google/{address}", name="maps_google")
      */
-    public function index()
+    public function mapsGoogle($address, GoogleMaps $googleMaps)
     {
+        $mapsDto = $googleMaps->search($address);
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MapsController.php',
+           'longitude' => $mapsDto->longitude,
+           'latitude' => $mapsDto->latitude,
+            'address' => $mapsDto->address
+        ]);
+    }
+
+    /**
+     * @Route("/maps/openstreet/{address}", name="maps_openstreet")
+     */
+    public function mapsOpenStreet($address, OpenStreetMaps $openStreetMaps)
+    {
+        $mapsDto = $openStreetMaps->search($address);
+
+        return $this->json([
+            'longitude' => $mapsDto->longitude,
+            'latitude' => $mapsDto->latitude,
+            'address' => $mapsDto->address
         ]);
     }
 }
